04bf34ec77971e9d9b17f11d4a7c5c3cd33b80a1
donatasjan5 <donatasjan5@gmail.com>
2023-10-15 14:53:00 +0300

    header links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
       </header>
       <header className="header-upper py-3">
         <div className="conatiner-xxl">
-          <div className="row">
+          <div className="row align-items-center">
             <div className="col-2 d-flex align-items-center">
               <h1>
                 <Link className="text-white text-decoration-none d-flex align-items-center">
@@ -34,20 +34,49 @@ const Header = () => {
               </h1>
             </div>
             <div className="col-5">
-              <div className="input-group mb-3">
+              <div className="input-group ">
                 <input
                   type="text"
-                  class="form-control"
-                  placeholder="Recipient's username"
-                  aria-label="Recipient's username"
+                  className="form-control py-2"
+                  placeholder="Search Product Here..."
+                  aria-label="Search Product Here..."
                   aria-describedby="basic-addon2"
                 />
-                <span className="input-group-text" id="basic-addon2">
-                  @example.com
+                <span className="input-group-text p-3" id="basic-addon2">
+                  <i className="bi bi-search fs-6"></i>
                 </span>
               </div>
             </div>
-            <div className="col-5"></div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <div>
+                    <Link className="text-decoration-none">
+                    <i className="bi bi-arrow-repeat"></i>
+                    <p>Compare <br /> Products</p>
+                    </Link>
+                </div>
+                <div>
+                    <Link>
+                    <i class="bi bi-heart"></i>
+                    <p>Favourite <br /> wishlist</p>
+                    </Link>
+                </div>
+                <div>
+                    <Link>
+                    <i class="bi bi-person"></i>
+                    <p>Log in <br /> My Account</p>
+                    </Link>
+                </div>
+                <div>
+                    <Link className="d-flex align-items-center gap-10">
+                    <i class="bi bi-cart"></i>
+                    <div className="d-flex flex-column">
+                        <span className="badge bg-white text-dark">0</span>
+                    </div>
+                    </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </header>
