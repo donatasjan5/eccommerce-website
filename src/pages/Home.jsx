cf76103baed49f4c8d613fc3fb74939b8903d7f3
donatasjan5 <donatasjan5@gmail.com>
2023-10-20 13:46:40 +0300

    Home section second part with icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,68 +24,119 @@ const Home = () => {
             </div>
             <div className="col-6">
               <div className="d-flex gap-10 flex-wrap justify-contentbetween align-items-center">
-
-              <div className="small-banner position-relative">
-                <img
-                  src="images/catbanner-01.jpg"
-                  className="img-fluid rounded-3"
-                  alt="banner"
-                />
-                <div className="small-banner-content position-absolute">
-                  <h4>SUPERCHARGED FOR PROS.</h4>
-                  <h5>iPad S13+ Pro.</h5>
-                  <p>Special Deal $999.99 or <br /> $27.51 per month</p>
-                  <Link className="button">BUY NOW</Link>
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-01.jpg"
+                    className="img-fluid rounded-3"
+                    alt="banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>SUPERCHARGED FOR PROS.</h4>
+                    <h5>iPad S13+ Pro.</h5>
+                    <p>
+                      Special Deal $999.99 or <br /> $27.51 per month
+                    </p>
+                    <Link className="button">BUY NOW</Link>
+                  </div>
                 </div>
-              </div>
 
-              <div className="small-banner position-relative">
-                <img
-                  src="images/catbanner-02.jpg"
-                  className="img-fluid rounded-3"
-                  alt="banner"
-                />
-                <div className="small-banner-content position-absolute">
-                  <h4>NEW ARRIVAL.</h4>
-                  <h5>Buy iPad Pro</h5>
-                  <p>Special Deal $999.99 or <br /> $27.51 per month</p>
-                  <Link className="button">BUY NOW</Link>
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-02.jpg"
+                    className="img-fluid rounded-3"
+                    alt="banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>NEW ARRIVAL.</h4>
+                    <h5>Buy iPad Pro</h5>
+                    <p>
+                      Special Deal $999.99 or <br /> $27.51 per month
+                    </p>
+                    <Link className="button">BUY NOW</Link>
+                  </div>
                 </div>
-              </div>
 
-              <div className="small-banner position-relative">
-                <img
-                  src="images/catbanner-03.jpg"
-                  className="img-fluid rounded-3"
-                  alt="banner"
-                />
-                <div className="small-banner-content position-absolute">
-                  <h4>SUPERCHARGED FOR PROS.</h4>
-                  <h5>iPad S13+ Pro.</h5>
-                  <p>Special Deal $999.99 or <br /> $27.51 per month</p>
-                  <Link className="button">BUY NOW</Link>
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-03.jpg"
+                    className="img-fluid rounded-3"
+                    alt="banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>SUPERCHARGED FOR PROS.</h4>
+                    <h5>iPad S13+ Pro.</h5>
+                    <p>
+                      Special Deal $999.99 or <br /> $27.51 per month
+                    </p>
+                    <Link className="button">BUY NOW</Link>
+                  </div>
                 </div>
-              </div>
 
-              <div className="small-banner position-relative">
-                <img
-                  src="images/catbanner-04.jpg"
-                  className="img-fluid rounded-3"
-                  alt="banner"
-                />
-                <div className="small-banner-content position-absolute">
-                  <h4>SUPERCHARGED FOR PROS.</h4>
-                  <h5>iPad S13+ Pro.</h5>
-                  <p>Special Deal $999.99 or <br /> $27.51 per month</p>
-                  <Link className="button">BUY NOW</Link>
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-04.jpg"
+                    className="img-fluid rounded-3"
+                    alt="banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>SUPERCHARGED FOR PROS.</h4>
+                    <h5>iPad S13+ Pro.</h5>
+                    <p>
+                      Special Deal $999.99 or <br /> $27.51 per month
+                    </p>
+                    <Link className="button">BUY NOW</Link>
+                  </div>
                 </div>
-              </div>
               </div>
             </div>
           </div>
         </div>
       </section>
-
+      <section className="home-wrapper-2 py-5">
+        <div className="conatiner-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="servies d-flex align-items-center justify-content-around">
+                <div className="d-flex align-items-center gap-10">
+                  <img src="images/service.png" alt="services" />
+                  <div>
+                    <h6>Free Shipping</h6>
+                    <p className="mb-0">From all orders over $5</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-10">
+                  <img src="images/service-02.png" alt="services" />
+                  <div>
+                    <h6>Daily Surprise Offers</h6>
+                    <p className="mb-0">Save up to 20%</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-10">
+                  <img src="images/service-03.png" alt="services" />
+                  <div>
+                    <h6>Support 24/7</h6>
+                    <p className="mb-0">Shop with an expert</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-10">
+                  <img src="images/service-04.png" alt="services" />
+                  <div>
+                    <h6>Affordable Prices</h6>
+                    <p className="mb-0">Get factory default price</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-10">
+                  <img src="images/service-05.png" alt="services" />
+                  <div>
+                    <h6>Secure Payments</h6>
+                    <p className="mb-0">100% Protected Payment</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
